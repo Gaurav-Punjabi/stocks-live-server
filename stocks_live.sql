1858dbd4b088a7b8b20b18f6bfe13b3fc8e3fc97
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2019-03-11 16:26:34 +0530

    Updated sql file

--- stocks_live.sql
+++ stocks_live.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 09, 2019 at 09:18 PM
+-- Generation Time: Mar 11, 2019 at 11:55 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 5.6.38
 
@@ -68,11 +68,8 @@ CREATE TABLE `StockCall` (
 --
 
 INSERT INTO `StockCall` (`id`, `stockId`, `typeId`, `price`, `stopLoss`, `target1`, `target2`, `target3`, `completed`, `deleted`, `createdAt`, `createdBy`, `updatedAt`) VALUES
-(1, 1, 1, 12.12, 13.12, 12.3, 13.21, 0, 0, 0, '2019-01-06 03:31:26', NULL, NULL),
-(2, 2, 1, 132.13, 13214, 13214, 13214, 13214, 0, 0, '2019-01-19 01:43:21', NULL, NULL),
-(3, 2, 1, 132.13, 13214, 13214, 13214, 13214, 0, 0, '2019-01-19 01:51:04', NULL, NULL),
-(4, 2, 1, 133.15, 231, 214, 1243, 1322, 0, 0, '2019-01-19 12:21:47', NULL, NULL),
-(5, 2, 1, 133.15, 150, 160, 170, 190, 0, 0, '2019-01-19 19:27:22', NULL, NULL);
+(1, 2, 1, 129.75, 132, 127, 130, 0, 0, 0, '2019-03-11 11:34:10', NULL, NULL),
+(2, 3, 1, 450.9, 452, 440, 430, 0, 0, 0, '2019-03-11 11:35:55', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -92,7 +89,11 @@ CREATE TABLE `StockInfo` (
 
 INSERT INTO `StockInfo` (`id`, `name`, `typeId`) VALUES
 (1, 'Zinc', 1),
-(2, 'ALUMINI', 1);
+(2, 'ALUMINI', 1),
+(3, 'COPPER', 1),
+(4, 'CRUDEOIL', 1),
+(5, 'GBPINR', 1),
+(6, 'EURINR', 1);
 
 -- --------------------------------------------------------
 
@@ -121,8 +122,29 @@ INSERT INTO `StockType` (`id`, `name`) VALUES
 CREATE TABLE `Users` (
   `id` int(11) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `notificationToken` varchar(255) DEFAULT NULL
+  `password` varchar(255) DEFAULT NULL,
+  `access_token` varchar(255) DEFAULT NULL,
+  `refresh_token` varchar(255) DEFAULT NULL,
+  `access_time` datetime DEFAULT NULL,
+  `refresh_time` datetime DEFAULT NULL,
+  `is_admin` varchar(255) DEFAULT NULL,
+  `deleted` tinyint(4) DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `notification_token` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(10) DEFAULT NULL,
+  `otp` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`id`, `email`, `password`, `access_token`, `refresh_token`, `access_time`, `refresh_time`, `is_admin`, `deleted`, `created_at`, `updated_at`, `created_by`, `updated_by`, `notification_token`, `phone_number`, `otp`) VALUES
+(1, 'gaurav@gmail.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', NULL, NULL, NULL, NULL, '1', 0, '2019-03-11 00:51:54', NULL, NULL, NULL, 'ExponentPushToken[OQzngxFCtzN7TuIACUVM6b]', '8828262618', '261542'),
+(3, 'gaurav.bpunjabi@gmail.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', NULL, NULL, NULL, NULL, '0', 0, '2019-03-11 15:25:17', NULL, NULL, NULL, 'ExponentPushToken[OQzngxFCtzN7TuIACUVM6b]', '9320010500', '302607');
 
 --
 -- Indexes for dumped tables
@@ -156,7 +178,9 @@ ALTER TABLE `StockType`
 -- Indexes for table `Users`
 --
 ALTER TABLE `Users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone_number` (`phone_number`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -172,13 +196,13 @@ ALTER TABLE `CallType`
 -- AUTO_INCREMENT for table `StockCall`
 --
 ALTER TABLE `StockCall`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `StockInfo`
 --
 ALTER TABLE `StockInfo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `StockType`
@@ -190,7 +214,7 @@ ALTER TABLE `StockType`
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
